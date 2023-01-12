94c9af4820e0e91373bc40649f577afffc12f67d
SandarvaShakya <sandarvashakya34@gmail.com>
2023-01-12 15:42:25 +0545

    🔨Update baseyrl in front-end

--- phonebook-frontend/src/services/persons.js
+++ phonebook-frontend/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://phonebook-api-ss.vercel.app/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
